c3eba8b63e3094f47eec3a5f333afe7bb01e72e2
Ti-Avanti <zds878933361@126.com>
2026-05-12 15:02:03 +0800

    Clean lifespan displays on entity removal

--- src/main/java/gg/fotia/fotiavillage/lifespan/LifespanDeathListener.java
+++ src/main/java/gg/fotia/fotiavillage/lifespan/LifespanDeathListener.java
@@ -1,10 +1,12 @@
 package gg.fotia.fotiavillage.lifespan;
 
 import gg.fotia.fotiavillage.FotiaVillagePlugin;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityRemoveEvent;
 
 public final class LifespanDeathListener implements Listener {
     private final FotiaVillagePlugin plugin;
@@ -15,7 +17,16 @@ public final class LifespanDeathListener implements Listener {
 
     @EventHandler
     public void onVillagerDeath(EntityDeathEvent event) {
-        if (event.getEntity() instanceof Villager villager && plugin.lifespan().hasLifespan(villager)) {
+        cleanup(event.getEntity());
+    }
+
+    @EventHandler
+    public void onVillagerRemove(EntityRemoveEvent event) {
+        cleanup(event.getEntity());
+    }
+
+    private void cleanup(Entity entity) {
+        if (entity instanceof Villager villager) {
             plugin.lifespan().cleanupDisplay(villager);
         }
     }
